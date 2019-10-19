cf23d03e3d66e8208f5044d7f238ad5a193e03fe
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-19 16:43:33 -0600

    tentando fazer rodar isso aqui!

--- web/Projects1/database.php
+++ web/Projects1/database.php
@@ -1,31 +1,31 @@
 <?php
 echo "2";
-function get_db() {
-  echo "db4"
-try
-{
-  echo "db7";
-  $dbUrl = getenv('DATABASE_URL');
-  echo "db9";
-  $dbOpts = parse_url($dbUrl);
+// function get_db() {
+//   echo "db4"
+// try
+// {
+//   echo "db7";
+//   $dbUrl = getenv('DATABASE_URL');
+//   echo "db9";
+//   $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+//   $dbHost = $dbOpts["host"];
+//   $dbPort = $dbOpts["port"];
+//   $dbUser = $dbOpts["user"];
+//   $dbPassword = $dbOpts["pass"];
+//   $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+//   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }
+// catch (PDOException $ex)
+// {
+//   echo 'Error!: ' . $ex->getMessage();
+//   die();
+// }
 
-echo "aqui!"
-return $db;
-}
+// echo "aqui!"
+// return $db;
+// }
 ?>
