fd52ef82718125b9b31a7dd725abfdbc38bb1e3c
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-19 19:09:40 -0600

    Trying hard =/

--- web/Projects1/products.php
+++ web/Projects1/products.php
@@ -16,25 +16,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <?php require_once('navbar.php'); ?>
-    <!-- Products -->
-    <?php
-      echo "25";
-          foreach ($db->query('
-  SELECT
-      guitar_name,
-      guitar_price,
-
-  FROM
-      GUITAR') as $row) {
-
-            echo'
-          <div class="d-flex flex-column ml-2">
-            <h5 class="card-title text-center">'. ucfirst($row['guitar_name']) . '</h5>
-            <a href="#" class="btn btn-primary btn-md ">See Description</a>
-          </div>';
-          }
-          ?>
     <section class="pricing py-5">
             <div class="container">
               <div class="row">
