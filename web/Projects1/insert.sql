aa0aa43d09bf27e47e35c0a46c9ea6b05a865b5c
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-10-23 15:47:50 -0600

    database working

--- web/Projects1/insert.sql
+++ web/Projects1/insert.sql
@@ -1,13 +1,17 @@
-INSERT INTO GUITAR(name, price) VALUES('StratoCaster', 399);
-INSERT INTO GUITAR(name, price) VALUES('TeleCaster', 349);
+INSERT INTO GUITAR(name, price) VALUES('Stratocaster', 399);
+INSERT INTO GUITAR(name, price) VALUES('Telecaster', 349);
 INSERT INTO GUITAR(name, price) VALUES('Jaguar', 299);
 
 INSERT INTO BODY(color) VALUES ('White');
 INSERT INTO BODY(color) VALUES ('Black');
 INSERT INTO BODY(color) VALUES ('Green');
 
-INSERT INTO PRODUCT_ORDER(guitar_id, color_id) VALUES (2,1);
-INSERT INTO PRODUCT_ORDER(guitar_id, color_id) VALUES (3,1);
-INSERT INTO PRODUCT_ORDER(guitar_id, color_id) VALUES (2,2);
-INSERT INTO PRODUCT_ORDER(guitar_id, color_id) VALUES (1,2);
-INSERT INTO PRODUCT_ORDER(guitar_id, color_id) VALUES (2,3);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Stratowhite.png', 1, 1);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Stratoblack.png', 1, 2);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Stratogreen.png', 1, 3);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Telewhite.png', 2, 1);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Teleblack.png', 2, 2);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Telegreen.png', 2, 3);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Jaguarwhite.png', 3, 1);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Jaguarblack.png', 3, 2);
+INSERT INTO GUITAR_BODY(img, guitar_id, color_id) VALUES ('Jaguargreen.png', 3, 3);
