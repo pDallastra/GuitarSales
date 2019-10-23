1dd6e0761e5f3bd1d6fd6caebeca6477b8f563b2
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-10-23 12:17:48 -0600

    trying

--- web/Projects1/mainAds.php
+++ web/Projects1/mainAds.php
@@ -1,4 +1,4 @@
-<div class="bd-example">
+<div style="background: linear-gradient(to right, #58508d, #494ca2);" class="bd-example">
   <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
