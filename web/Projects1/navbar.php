d70d268fed597b80388779e1b712f6e499d34e0c
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-10-23 12:04:21 -0600

    trying

--- web/Projects1/navbar.php
+++ web/Projects1/navbar.php
@@ -12,7 +12,7 @@
         <a class="nav-link" href="story.php">Our Story</a>
       </li> -->
       <li class="nav-item">
-        <a class="nav-link" href="productTest.php">Products</a>
+        <a class="nav-link" href="products.php">Products</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Chart</a>
