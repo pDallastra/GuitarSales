f165939fca0f04ec66fe9079bc748ceffaf1fbea
Paulo Dallastra <paulodallastra@Paulos-MacBook-Pro.local>
2019-10-23 22:10:15 -0600

    database working

--- web/Projects1/products.php
+++ web/Projects1/products.php
@@ -38,7 +38,7 @@ $db = get_db();
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Guitar Model: ' . ucfirst($row['name']) . '</li>
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Body Color: ' . ucfirst($row['color']) . '</li>
                   </ul>
-                  <button type="submit" name="'. ucfirst($row) . '" href="test.php" class="btn btn-block btn-primary text-uppercase">Add to Cart</button>
+                  <button type="submit" name="'. $row . '" href="test.php" class="btn btn-block btn-primary text-uppercase">Add to Cart</button>
                 </div>
               </div>
             </div>
