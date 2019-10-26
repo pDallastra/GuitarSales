74352f4cd9a2a4369eb300e6792dfcc394af3c4a
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-26 12:46:43 -0600

    testing

---
+++ web/Projects1/confirmation.php
@@ -0,0 +1,54 @@
+<?php
+// get the data from the POST
+$name = $_POST['txtName'];
+$email = $_POST['txtEmail'];
+$model = $_POST['txtModel'];
+$color = $_POST['txtColor'];
+
+if($model == 'Stratocaster')
+{
+    $price = 399;
+}
+else if($model == 'Telecaster')
+{
+    $price = 349;
+}
+else if($model == 'Jaguar')
+{
+    $price = 299;
+}
+
+require("database.php");
+$db = get_db();
+
+try
+{
+   $query = 'INSERT INTO product_order(name, email, model, color, price) VALUES(:name, :email, :model, :color, :price)';
+   $statement = $db->prepare($query);
+   $statement->bindValue(':name', $name);
+   $statement->bindValue(':title', $title);
+   $statement->bindValue(':model', $model);
+   $statement->bindValue(':color', $color);
+   $statement->bindValue(':price', $price);
+   $statement->execute();
+
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+
+// finally, redirect them to a new page to actually show the topics
+header("Location: notes.php?courseId=$course");
+
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
+
+?>
