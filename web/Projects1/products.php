b366e146b9e6e57e8c5913edcdfe409edc892cbb
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-26 11:26:14 -0600

    testing

--- web/Projects1/products.php
+++ web/Projects1/products.php
@@ -54,46 +54,28 @@ $db = get_db();
               <form>
                   <div class="form-row">
                     <div class="form-group col-md-6">
-                      <label for="inputEmail4">Email</label>
-                      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                      <label for="inputEmail4">Full Name</label>
+                      <input type="text" class="form-control" id="txtName" name="txtName" placeholder="Full Name">
                     </div>
                     <div class="form-group col-md-6">
-                      <label for="inputPassword4">Password</label>
-                      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                      <label for="inputPassword4">Email</label>
+                      <input type="email" class="form-control" id="txtEmail" name="txtEmail" placeholder="Email">
                     </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                  </div>
-                  <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
                   </div>
                   <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label for="inputCity">City</label>
-                      <input type="text" class="form-control" id="inputCity">
-                    </div>
-                    <div class="form-group col-md-4">
-                      <label for="inputState">State</label>
-                      <select id="inputState" class="form-control">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                      </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                      <label for="inputZip">Zip</label>
-                      <input type="text" class="form-control" id="inputZip">
-                    </div>
-                  </div>
+                  <select id="txtModel" class="form-control">
+                        <option selected>Guitar Model</option>
+                        <option>Stratocaster</option>
+                        <option>Telecaster</option>
+                        <option>Jaguar</option>
+                  </select>
+                  <select id="txtModel" class="form-control">
+                        <option selected>Body Color</option>
+                        <option>White</option>
+                        <option>Black</option>
+                        <option>Green</option>
+                  </select>
                   <div class="form-group">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" id="gridCheck">
-                      <label class="form-check-label" for="gridCheck">
-                        Check me out
-                      </label>
-                    </div>
                   </div>
                   <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
