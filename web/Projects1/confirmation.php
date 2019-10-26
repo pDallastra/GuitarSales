ff3e0d5b44469787672b8c79abc722403f6d429f
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-26 13:51:46 -0600

    testing

--- web/Projects1/confirmation.php
+++ web/Projects1/confirmation.php
@@ -57,7 +57,7 @@ die(); // we always include a die after redirects. In this case, there would be 
                     <div class="container">
                       <div class="row">
     <?php
-          foreach ($db->query('SELECT TOP 1 * FROM PRODUCT_ORDER BY id DESC') as $row) 
+          foreach ($db->query('SELECT * FROM PRODUCT_ORDER') as $row) 
           {
 
             echo '
@@ -68,15 +68,14 @@ die(); // we always include a die after redirects. In this case, there would be 
                   <hr>
                   <ul class="fa-ul">
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Name: <strong>' . ucfirst($row['name']) . '</strong></li>
-                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Email: <strong>' . ucfirst($row['email']) . '</strong></li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Email: <strong>' . $row['email'] . '</strong></li>
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Model: <strong>' . ucfirst($row['model']) . '</strong></li>
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Color: <strong>' . ucfirst($row['color']) . '</strong></li>
                     <li><span class="fa-li"><i class="fas fa-check"></i></span>Price: $ <strong>' . ucfirst($row['price']) . '</strong></li>                     
                     </ul>
                 </div>
               </div>
-            </div>
-            ';
+            </div>';
           }
 ?>
     </div>
