52ae44ae3dd0adb5bbf94cb89e6604e5c10633b8
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-26 23:47:40 -0600

    style changes

--- web/Projects1/products.php
+++ web/Projects1/products.php
@@ -16,9 +16,9 @@ $db = get_db();
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #315b96;">
     <?php require_once('navbar.php'); ?>
-    <section class="pricing py-5" style="font-size: 14pt; font-family: Tahoma;background-color: #315b96;">
+    <section class="pricing py-5" style="font-size: 14pt; font-family: Tahoma;">
                     <div class="container">
                       <div class="row">
     <?php
