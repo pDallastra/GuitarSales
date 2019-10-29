4d39225c85f85badd6ea44c6cf585f85b3062357
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-10-29 17:52:49 -0600

    styling

--- web/Projects1/products.php
+++ web/Projects1/products.php
@@ -39,9 +39,9 @@ $db = get_db();
                   <hr>
                   <ul class="fa-ul" style="list-style-type:none">
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Guitar Model: <strong>' . ucfirst($row['name']) . '</strong></li>
-                  <input type="hidden" id="txtModel" name="textModel" value="' . $model . '"/>
+                  <input type="hidden" id="txtModel" name="textModel" value="' $model '"/>
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Body Color: <strong>' . ucfirst($row['color']) . '</strong></li>
-                  <input type="hidden" id="txtColor" name="txtColor" value="' $color . '"/>
+                  <input type="hidden" id="txtColor" name="txtColor" value="' $color '"/>
                   </ul>
                 </div>
                 
