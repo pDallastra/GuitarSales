1245d57cd7b29ee72164fcc6749603b26945dd52
pDallastra <52924113+pDallastra@users.noreply.github.com>
2019-11-02 18:53:13 -0600

    Final Tables
    Tables Finished.

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,42 +1,30 @@
-CREATE DATABASE guitarSales;
-
-CREATE TABLE PRODUCT(
-    product_id          SERIAL          NOT NULL PRIMARY KEY 
-,   product_name        varchar(50)     NOT NULL  
-
-,   bodyColor_color          varchar(50)     NOT NULL
-,   shieldColor_color        varchar(50)     NOT NULL
-,   pickUpColor_color        varchar(50)     NOT NULL   
+CREATE TABLE GUITAR(
+    id          SERIAL          NOT NULL PRIMARY KEY,
+    name        VARCHAR(20)     NOT NULL UNIQUE,
+    price       INT             NOT NULL
 
 );
 
-CREATE TABLE BODY_COLOR(
-    bodyColor_id            SERIAL          NOT NULL PRIMARY KEY
-,   body_color              varchar(50)     NOT NULL
+CREATE TABLE BODY(
+    id          SERIAL          NOT NULL PRIMARY KEY,
+    color       VARCHAR(20)     NOT NULL UNIQUE
+);
+
+CREATE TABLE GUITAR_BODY(
+    id          SERIAL          NOT NULL PRIMARY KEY,
+    img         TEXT            NOT NULL,
+
+    guitar_id   INT             NOT NULL REFERENCES guitar(id),
+    color_id    INT             NOT NULL REFERENCES body(id)
 
 );
 
-CREATE TABLE SHIELD_COLOR(
-    shieldColor_id          SERIAL          NOT NULL PRIMARY KEY
-,   shield_color            varchar(50)     NOT NULL
+CREATE TABLE PRODUCT_ORDER(
+    id          SERIAL          NOT NULL PRIMARY KEY,
 
-);
-
-CREATE TABLE PICKUP_COLOR(
-    pickUpColor_id          SERIAL          NOT NULL PRIMARY KEY
-,   shield_color            varchar(50)     NOT NULL
-
-);
-
-CREATE TABLE CUSTOMER(
-    customer_id             SERIAL          NOT NULL PRIMARY KEY
-,   customer_name           varchar(50)     NOT NULL
-
-);
-
-CREATE TABLE ORDER(
-    order_id                SERIAL          NOT NULL PRIMARY KEY
-,   order_date              date            NOT NULL
-
-,   customer_id       	    INT		    NOT FULL
+    name        TEXT            NOT NULL,
+    email       TEXT            NOT NULL,
+    model       VARCHAR(20)     NOT NULL,
+    color       VARCHAR(20)     NOT NULL,
+    price       INT             NOT NULL
 );
